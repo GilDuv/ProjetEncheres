09b6d254f3f15663b08e15fccf12002a6dd9dce9
GilDuv <gilles.duverd2023@campus-eni.fr>
2024-02-28 14:42:10 +0100

    ajouts utilisateurRepositoryImpl

--- src/main/java/fr/eni/projetEnchere/dal/UtilisateurRepository.java
+++ src/main/java/fr/eni/projetEnchere/dal/UtilisateurRepository.java
@@ -3,6 +3,7 @@ package fr.eni.projetEnchere.dal;
 import java.util.List;
 import java.util.Optional;
 
+import fr.eni.projetEnchere.bo.Article;
 import fr.eni.projetEnchere.bo.Utilisateur;
 
 public interface UtilisateurRepository {
@@ -11,11 +12,19 @@ public interface UtilisateurRepository {
 	
 	public Optional<Utilisateur> findProfilById(Integer id);
 	
+	public Utilisateur findProfilByPseudo(String pseudo);
+	
 	public Utilisateur creerProfil(Utilisateur utilisateur);
 	
 	public Utilisateur modifierProfil(Utilisateur utilisateur);
 	
 	public void supprimerProfil(Integer idUtilisateur);
+	
+	public List<Article> getAllArticlesVendus(Utilisateur utilisateur);
+	
+	public List<String> getAllPseudos();
+	
+	
 	
 	
 }
